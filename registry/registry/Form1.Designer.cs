4259cf106796983c59f1dc7232a15af6d4bf3a97
VladyslavSH <manujlenkovlad2@gmail.com>
2018-10-23 01:43:05 +0300

    Work is finished

--- registry/registry/Form1.Designer.cs
+++ registry/registry/Form1.Designer.cs
@@ -32,6 +32,13 @@
             this.Show = new System.Windows.Forms.Button();
             this.Add = new System.Windows.Forms.Button();
             this.Delete = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // listView1
@@ -41,10 +48,11 @@
             this.listView1.Size = new System.Drawing.Size(746, 465);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
             // 
             // Show
             // 
-            this.Show.Location = new System.Drawing.Point(777, 12);
+            this.Show.Location = new System.Drawing.Point(794, 12);
             this.Show.Name = "Show";
             this.Show.Size = new System.Drawing.Size(75, 23);
             this.Show.TabIndex = 1;
@@ -54,33 +62,94 @@
             // 
             // Add
             // 
-            this.Add.Location = new System.Drawing.Point(777, 41);
+            this.Add.Location = new System.Drawing.Point(794, 41);
             this.Add.Name = "Add";
             this.Add.Size = new System.Drawing.Size(75, 23);
             this.Add.TabIndex = 2;
             this.Add.Text = "Add";
             this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
             // 
             // Delete
             // 
-            this.Delete.Location = new System.Drawing.Point(777, 70);
+            this.Delete.Location = new System.Drawing.Point(794, 70);
             this.Delete.Name = "Delete";
             this.Delete.Size = new System.Drawing.Size(75, 23);
             this.Delete.TabIndex = 3;
             this.Delete.Text = "Delete";
             this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(764, 349);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(132, 128);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "AddProperties";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name Program";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Value Program(*)";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(22, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "* - optional";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(877, 489);
+            this.ClientSize = new System.Drawing.Size(908, 489);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.Delete);
             this.Controls.Add(this.Add);
             this.Controls.Add(this.Show);
             this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -91,6 +160,12 @@
         private System.Windows.Forms.Button Show;
         private System.Windows.Forms.Button Add;
         private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- registry/registry/Form1.cs
+++ registry/registry/Form1.cs
@@ -20,12 +20,67 @@ namespace registry
 
         private void Show_Click(object sender, EventArgs e)
         {
+            ShowReg();
+            
+        }
+
+        private void ShowReg()
+        {
             listView1.Clear();
             RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
             foreach (var item in registryKey.GetValueNames())
             {
                 listView1.Items.Add(item);
             }
+            registryKey = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+            foreach (var item in registryKey.GetValueNames())
+            {
+                listView1.Items.Add(item);
+            }
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != "")
+                {
+                    RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+                    registryKey.SetValue(textBox1.Text, textBox2.Text);
+                    
+                }
+                else MessageBox.Show("Please enter Name Program!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                ShowReg();
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+            
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+                registryKey.DeleteValue(listView1.SelectedItems[0].Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                ShowReg();
+            }
+            
         }
     }
 }
